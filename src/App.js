851561905eb5fe312f2661f1bb75df978f896b83
raonemota <raonemota@hotmail.com>
2020-06-21 20:51:54 -0300

    Start project

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+import './App.css';
+import backgroundImage from './assets/background.jpg'
+
+import Header from './components/Header'
+
+function App(){
+
+    const [projects, setProjects] = useState(['Desenvolvimento de App', 'Front-end web'])
+    
+    function handleAddProject(){
+        setProjects([...projects, `Novo Projeto ${Date.now()}` ])
+    }
+    
+    return(
+        <>           
+            <Header title="Projects"/>
+
+            <img width={300} src={backgroundImage}/>
+
+            <ul>
+                {projects.map(project => <li key={project}>{project}</li>)}
+            </ul>
+
+            <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
+        </>
+    );
+}
+
+export default App;
